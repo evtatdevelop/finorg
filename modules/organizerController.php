ef1f458bf76dc316df15046ff179251803adb514
Evgenii <evtatdevelop@gmail.com>
2022-07-13 14:25:18 +0700

    regular events base functional

--- modules/organizerController.php
+++ modules/organizerController.php
@@ -2,54 +2,66 @@
     require_once( 'db/mysql.php'           );
     require_once( './auxes/validation.php' );
 
-    function getEvents( $props ) {       
-        $props['data'] = ['id', 'date', 'name', 'description', 'type', 'value', 'status', 'cash', 'mode'];
-        $events = select( $props );
-
-        $regulars = [];
-        $monthFrom   = (int) $props['from'];
-        $monthTo     = (int) $props['to'];
-        foreach ( getRegulars() as $regEvent ) {
-           $regEventFrom   = (int) $regEvent['date_from'];
-           $regEventTo     = (int) $regEvent['date_to'];
-           $regEventPeriod = (string) $regEvent['period'];
-           $ms = 24*60*60*1000;
-           $steps = ['year' => 365 * $ms, 'month' => 30 * $ms, 'week' => 7 * $ms, 'day' => 1 * $ms, ];
-
-           $date = $regEventFrom;
-
-           while ( $date < $monthTo ) { 
-            if ( $date >= $monthFrom ) array_push($regulars, [
-                    "id" => $regEvent['id'] .'-'. $date,
-                    "date" => $date,
-                    "name" => $regEvent['name'],
-                    "description" => $regEvent['description'],
-                    "type" => $regEvent['type'],
-                    "value" => $regEvent['value'],
-                    "status" => $regEvent['status'],
-                    "cash" => $regEvent['cash'],
-                    "mode" => $regEvent['mode'],
-                ]);
-                $date += $steps[$regEventPeriod];
-            } ;
-
-
+    function getNextDate( $date, $period ) {
+        date_default_timezone_set('Asia/Bangkok');
+        $dateArr = getdate($date / 1000);
+        $year   = $dateArr['year'];
+        $month  = $dateArr['mon'];
+        $day    = $dateArr['mday'];
+        switch ( $period ) {
+            case 'year':    $year  += 1; break;
+            case 'month':   $month += 1; break;
+            case 'week':    $day   += 7; break;
+            case 'day':     $day   += 1; break;
+            default: $day += (int) $period;
         }
-
-        
-        // dump($regulars);
-        // dump($events);
-        return $regulars;
-
-        return $events;
-        // return select( $props );
+        return mktime( $dateArr['hours'], $dateArr['minutes'], $dateArr['seconds'], $month, $day, $year ) * 1000;
     }
 
-    function getRegulars() {
+    function getEvents( $props ) {
+        $events = getOneimeEvents( $props );
+        $regulars = getRegulars( $props );
+        $eventsArr = array_merge($events, $regulars);
+        usort( $eventsArr, function($a, $b){ return ($a['date'] - $b['date']); });
+        return $eventsArr;
+    }
+
+    function getOneimeEvents( $props ) {
+        $props['data'] = ['id', 'date', 'name', 'description', 'type', 'value', 'status', 'cash', 'mode'];
+        return select( $props );       
+    }
+
+    function getRegulars( $props ) {
+        $result = [];
+        $monthFrom   = (int) $props['from'];
+        $monthTo     = (int) $props['to'];
         $props['q'] = 'regulars';
         $props['data'] = ['id', 'name', 'code', 'date_from', 'date_to', 'period', 'type', 'value', 'cash', 'description', 'status', 'mode'];
         $props['where'] = "status = 'active'";
-        return select( $props );
+        unset( $props['from'] );
+        unset( $props['to'] );
+
+        foreach ( select( $props ) as $regEvent ) {
+           $regEventFrom   = (int) $regEvent['date_from'];
+           $regEventTo     = (int) $regEvent['date_to'];
+           $regEventPeriod = (string) $regEvent['period'];
+           $date = $regEventFrom;
+           while ( $date <= $monthTo ) { 
+            if ( $date >= $monthFrom ) array_push($result, [
+                "id" => $regEvent['id'] .'-'. $date,
+                "date" => $date,
+                "name" => $regEvent['name'],
+                "description" => $regEvent['description'],
+                "type" => $regEvent['type'],
+                "value" => $regEvent['value'],
+                "status" => $regEvent['status'],
+                "cash" => $regEvent['cash'],
+                "mode" => $regEvent['mode'],
+            ]);
+            $date = getNextDate( $date, $regEventPeriod);
+            }
+        }
+        return $result;
     }
 
     function setEvent( $props ) {
